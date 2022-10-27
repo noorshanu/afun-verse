d9a159dcb16e819209689696d1f56a12a2e1ae29
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-10-28 02:29:45 +0530

    e

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <footer className='footer'>
+        <div class="container">
+        <hr/>
+            <div class="row justify-content-center align-items-center g-2">
+                <div class="col-md-6">Column</div>
+                <div class="col-md-6">Column</div>
+                <div class="col-md-6">Column</div>
+                <div class="col-md-6">Column</div>
+            </div>
+            <div className='foot-bottom'>
+                <hr/>
+                <span>2022© AFUN Inc. | All Rights Reserved </span>
+                
+            </div>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
