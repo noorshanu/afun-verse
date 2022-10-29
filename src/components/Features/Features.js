c3a8859f0a93565b03fa1f55814a0ad6bfe25a89
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-10-29 13:50:55 +0530

    bnv

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,5 +1,16 @@
 // import React, { useRef, useState } from "react";
 import Way from "../../assets/elements/way.svg";
+import Game from '../../assets/slider/game.png'
+import Art from '../../assets/slider/art.png'
+import Cars from '../../assets/slider/cars.png'
+import Fashion from '../../assets/slider/fashion.png'
+import Idols from '../../assets/slider/idols.png'
+import Music from '../../assets/slider/music.png'
+// import Art from '../../assets/slider/art.png'
+// import Art from '../../assets/slider/art.png'
+// import Art from '../../assets/slider/art.png'
+// import Art from '../../assets/slider/art.png'
+
 import "./Play.css";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -33,16 +44,35 @@ function Features() {
           className="mySwiper"
         >
           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" />
+            <img src={Game} alt="" />
+            <div className="contents">
+            <h1>Players can enjoy a variety of games at Game Arcade in AFUN verse. Most of the games are casual and easy to play.
+Users can have fun to play together with their friends or compete each other.</h1>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" />
+            <img src={Music} alt="" />
+            <div className="contents">
+            <h1>Music lovers can use Native token to buy a ticket to the music concert.
+At AFUN verse, world famous musicians will play concerts once a month and daily live music by emerging artists all around the world.
+Qualified $AFUC token holders have a free access to daily concerts and early access to purchase tickets of celebrity concerts.</h1>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" />
+            <img src={Art} alt="" />
+            <div className="contents">
+            <h1>Players can visit the art gallery and purchase NFT arts. They can also sell their own NFT arts. <br/>
+NFT market place ensures artists the ownership of the art works by blockchain technologies.</h1>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" />
+            <img src={Fashion} alt="" />
+            <div className="contents">
+            Personalise your avatar!<br/>
+Millions of NFT avatar items are available to purchase with GCOINs in AFUN verse.
+NFT items can also be won by the competions in AFUN Games, and also can be sold.
+Qualified $AFUNC token holders get free exclusive NFT avatar accessories occasionally.
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
