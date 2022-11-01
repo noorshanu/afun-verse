3607b7efa3673bc167390220dec678a12efd0b66
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-01 13:59:56 +0530

    fbfb

--- src/components/Roadmap/Roadmap.js
+++ src/components/Roadmap/Roadmap.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Roadmap.css";
-import Ps1 from '../../assets/p1.png'
-import Ps2 from '../../assets/p2.png'
-import Ps3 from '../../assets/p3.png'
+import Ps1 from "../../assets/p1.png";
+import Ps2 from "../../assets/p2.png";
+import Ps3 from "../../assets/p3.png";
 
 function Roadmap() {
   return (
@@ -13,37 +13,66 @@ function Roadmap() {
         data-aos-anchor-placement="center-center"
         data-aos-duration="1500"
       >
-        <h1 className="header-h1">
-          RoadMap
-        </h1>
+        <h1 className="header-h1">RoadMap</h1>
       </div>
 
       <div className="container">
         <div className="side-road">&nbsp;</div>
         <div className="side-road2">&nbsp;</div>
         <div className="row margin-auto ">
-        
-<div className="wrapper">
- 
-  <div className="timeline">
-    <dl className="timeline--entry">
-      <dt className="timeline--entry__title">
-        <img src={Ps1} alt="" className="team-box-1"/>
-      </dt>
-      <dd className="timeline--entry__detail">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</dd>
-    </dl>
-    <dl className="timeline--entry">
-      <dt className="timeline--entry__title">Title B</dt>
-      <dd className="timeline--entry__detail"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</dd>
-    </dl>
-    <dl className="timeline--entry">
-      <dt className="timeline--entry__title">Title C</dt>
-      <dd className="timeline--entry__detail">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</dd>
-    </dl>
-   
- 
-  </div>
-</div>
+          <div className="wrapper">
+            <div className="timeline">
+              <dl className="timeline--entry">
+                <dt className="timeline--entry__title">
+                  <img src={Ps1} alt="" className="team-box-1" />
+                  <div className="d-flex gap-4">
+                    <h2>Q4</h2>2022
+                  </div>
+                </dt>
+                <dd className="timeline--entry__detail">
+                  <p>Core business plan established</p>
+                  <p>$Afun SEED SALE </p>
+                  <p>Gameplay teasers</p>
+                  <p>Game alpha version launched</p>
+                </dd>
+              </dl>
+              <dl className="timeline--entry">
+                <dt className="timeline--entry__title">
+                  <img src={Ps2} alt="" className="team-box-1" />
+                  <div className="d-flex gap-4">
+                    <h2>Q1</h2>2023
+                  </div>
+                </dt>
+                <dd className="timeline--entry__detail">
+                  <p>PreSale Afun Token </p>
+                  <p>NFT launch </p>
+                  <p>Listing on CMC & CG </p>
+                  <p>  Conceptualization staking <br/> platform and token</p>
+                  <p>
+                   Wallet + smart
+                    contract <br/>implementation in game
+                  </p>
+                  <p>Game(Demo version) release</p>
+                  <p>Creating market place Afun</p>
+                </dd>
+              </dl>
+              <dl className="timeline--entry">
+                <dt className="timeline--entry__title">
+                  <img src={Ps3} alt="" className="team-box-1" />
+                  <div className="d-flex gap-4">
+                    <h2>Q2</h2>2023
+                  </div>
+                </dt>
+                <dd className="timeline--entry__detail">
+               <p>Launch of Staking Platform </p>
+               <p>Cross-chain development</p>
+               <p>Game mode enhancement</p>
+               <p>Listing on Dex and exchanges</p>
+               
+                </dd>
+              </dl>
+            </div>
+          </div>
         </div>
       </div>
     </section>
